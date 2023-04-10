e63219c95d49e75414197d95b31618db0bae6a7d
ElianRivoira <eli_rivoira@hotmail.com>
2023-04-10 10:49:51 -0300

    added cors

--- api/config/db/index.js
+++ api/config/db/index.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize('tmdb', 'Elian', '3214', {
+const db = new Sequelize(process.env.db, process.env.username, process.env.password, {
   dialect: "postgres",
-  host: "localhost",
-  port: 5432,
+  host: process.env.hostname,
+  port: process.env.port,
   logging: false
 });
 
--- api/server.js
+++ api/server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors= require("cors");
 
 const app = express();
+
+require('dotenv').config();
 
 const routes = require("./routes");
 const db = require("./config/db");
 
-const PORT = 3001;
-
 app.use(express.json());
 app.use(cookieParser())
-
+app.use(cors({
+  // Si aún no tenes deployado tu front en origin va la url local.
+  // Una vez que se deploye el front acá va esa url que te entrega.
+  origin: "https://tmdatabase.vercel.app",
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], credentials: true,
+}));
 app.use(morgan("tiny"));
 app.use(express.static(__dirname + "/public"));
 
@@ -23,7 +29,7 @@ app.use("/*", function (req, res) {
 });
 
 db.sync({force:false}).then(() => {
-  app.listen(PORT, () => {
-    console.log("Escuchando en el puerto ", PORT);
+  app.listen(process.env.port, () => {
+    console.log("Escuchando en el puerto ", process.env.port);
   });
 });
